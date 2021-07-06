e2044d58e2e4dd04b7aabcdd09701bb8183f5f00
Dawid Paszko <dawid.paszko@gmail.com>
2021-07-06 07:50:03 +0200

    Use env vars for db connection

--- api/db.js
+++ api/db.js
@@ -3,10 +3,10 @@ import knex from "knex";
 const db = knex({
   client: 'mysql',
   connection: {
-    host: 'localhost',
-    user: 'root',
-    password: 'root',
-    database: 'stackovercloned',
+    host: process.env.db_host || 'localhost',
+    user: process.env.db_user || 'root',
+    password: process.env.db_pass || 'root',
+    database: process.env.db_name || 'stackovercloned',
   },
 });
 
